acd7cf4c1f9bf7a115edc231602f0b62da2a6be2
Michal Jiránek <michal.jiranek00@gmail.com>
2022-11-28 18:44:45 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import React from "react";
+
 
 function App() {
+
+  const [items, setItems] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/', {
+      method: 'GET',
+      withCredentials: true,
+      crossorigin: true,
+      mode: 'no-cors',
+    })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+      </div>
   );
 }
+
+
 
 export default App;
---
+++ src/Server/DataLoad.js
@@ -0,0 +1,59 @@
+const {MongoClient} = require('mongodb')
+
+
+
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://tdsaprojekt:tdsaprojekt@tdsaprojekt.7z5lfwd.mongodb.net/?retryWrites=true&w=majority";
+
+
+    const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+        await  findCat(client, "kočka");
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+async function listDatabases(client){
+    let databasesList;
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+}
+
+let selected = [];
+
+async function findCat(client, findingValue){
+
+    selected = [await client.db("hra").collection("animals").findOne({name: findingValue}), await client.db("hra").collection("animals").findOne({name: "pes"})];
+
+
+    console.log(selected[1].photo);
+}
+
+app.get("/database", function(req, res) {
+    res.send(selected);
+});
+let port = process.env.PORT;
+if(port == null || port == "") {
+    port = 5000;
+}
+app.listen(port, function() {
+    console.log("Server started successfully");
+});
+
+main().catch(console.error);
